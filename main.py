abe4420e96429c34f0b6dcb9e66fab6a8e7f1eb9
AntonSinica <akb98@yandex.ru>
2025-02-17 15:08:58 +0300

    v.pre 3.1 + работа с секундами как целыми числами; + пропуск отдыха в финальном цикле.

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import time
 from plyer import notification
 from tqdm import tqdm
 
-def countdown(minutes):
-    for _ in tqdm(range(minutes * 60)):  # Прогресс-бар на minutes * 60 секунд
-        time.sleep(1)  # Ждем 1 секунду
+def countdown(seconds):
+    for _ in tqdm(range(seconds)):  # Прогресс-бар
+        time.sleep(1)
 
-def pomodoro_timer(work_minutes=1, break_minutes=1, cycles=2):
+def pomodoro_timer(work_minutes=0.1, break_minutes=0.1, cycles=2):
     for cycle in range(1, cycles + 1):
         print(f"Цикл {cycle}: Начинаем работу!")
-        countdown(work_minutes)  # Заменяем time.sleep на countdown
+        countdown(int(work_minutes * 60))
         notification.notify(
             title="Пора отдохнуть!",
             message=f"Цикл {cycle} завершен. Отдохните {break_minutes} минут.",
             timeout=10
         )
+        if cycle == cycles:  # на последнем цикле пропускаем цикл отдыха
+            continue
+
         print(f"Цикл {cycle}: Пора отдохнуть!")
-        countdown(break_minutes)  # Заменяем time.sleep на countdown
+        countdown(int(work_minutes * 60))  # Заменяем time.sleep на countdown
         notification.notify(
             title="Время работать!",
             message="Отдых окончен. Время вернуться к работе.",
